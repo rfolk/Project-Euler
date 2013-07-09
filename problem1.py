ccbe725e037dd320320d41600ec9a000c7e94c06
Russell Folk <rfolk@russellfolk.com>
2013-07-08 23:26:13 -0600

    Changed variable names to better reflect their purpose.
    end changed to stop for stopping point
    num changed to count for current count

--- problem1.py
+++ problem1.py
@@ -5,37 +5,37 @@
 # Project Euler #1
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-# end is 990 because the cycle will not complete before 1000.
-end   = 990
+# stop is 990 because the cycle will not complete before 1000.
+stop   = 990
 
 total = 0
-num   = 0
+count = 0
 
 # Could be done with mod 3 and 5 but will be less efficient
-while num < end :
-  num += 3
-  total += num
-  num += 2
-  total += num
-  num += 1
-  total += num
-  num += 3
-  total += num
-  num += 1
-  total += num
-  num += 2
-  total += num
-  num += 3
-  total += num
+while count < stop :
+  count += 3
+  total += count
+  count += 2
+  total += count
+  count += 1
+  total += count
+  count += 3
+  total += count
+  count += 1
+  total += count
+  count += 2
+  total += count
+  count += 3
+  total += count
 
-num += 3
-total += num
-num += 2
-total += num
-num += 1
-total += num
-num += 3
-total += num
+count += 3
+total += count
+count += 2
+total += count
+count += 1
+total += count
+count += 3
+total += count
 
 print ( "The total of all the multiples of 3 and 5 added together is: "
   + str ( total ) )
